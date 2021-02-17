23371fb9f0f1db737179c6750755a9823df218af
sebcc32 <sebascc28@javerianacali.edu.co>
2021-02-16 21:52:36 -0500

    segunda parte

--- calentamiento/2.cpp
+++ calentamiento/2.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
+int ingresarEdad(int edad){
+    cin>>edad;
+    return edad;
+}
 
+float ingresarAltura(float altura){
+    cin>>altura;
+    return altura;
+}
+
+string ingresarSexo(string sexo){
+    getchar();
+    getline(cin,sexo);
+    return sexo;
+}
 
 int main(){
-
+    string sexo;
+    float altura;
+    int edad;
+    cout<<"Digite su edad: ";
+    edad=ingresarEdad(edad);
+    cout<<"Digite su sexo: ";
+    sexo=ingresarSexo(sexo);
+    cout<<"Digite su altura: ";
+    altura=ingresarAltura(altura);
+    cout<<"Usted es de "<<edad<<" años de edad, con "<<altura<<" metros de sexo "<<sexo<<endl;
     return 0;
 }
